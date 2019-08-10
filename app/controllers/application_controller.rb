0f45a41c20735f90f3c3031ad7158d2719704a17
mbarrins <maija.barrins@gmail.com>
2019-08-10 11:26:09 +0100

    corrected application controller change"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
-  skip_before_action :authorize, only: [:fallback_index_html]
-  before_action :set_current_user
+  before_action :set_current_user, except: [:fallback_index_html]
   before_action :authorize
 
   def issue_token(payload)
